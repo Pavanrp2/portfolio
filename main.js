33140b97e3e8033bf3d0e24887f4354abb69fecb
Pavanrp2 <pavanrpatil2@gmail.com>
2024-02-14 19:02:21 +0530

    initial Project

---
+++ main.js
@@ -0,0 +1,29 @@
+// ------------home section--------
+let homeLink = document.getElementById("home-link");
+let home = document.getElementById("home-section");
+
+homeLink.addEventListener("click", function(event){
+    event.preventDefault();
+    home.scrollIntoView({behavior:'smooth'});
+})
+
+// ------------project section--------
+let projectLink = document.getElementById("project-link");
+let project = document.getElementById("project-section");
+
+projectLink.addEventListener("click", function (event){
+    event.preventDefault();
+    project.scrollIntoView({behavior: "smooth"});
+})
+
+
+// ------------contact section--------
+let contactLink = document.getElementById("contact-link");
+let contact = document.getElementById("contact-section");
+
+contactLink.addEventListener("click", function(event){
+    event.preventDefault();
+    contact.scrollIntoView({behavior:"smooth"});
+})
+
+
